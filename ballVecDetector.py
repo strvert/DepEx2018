5658a70caa15890cd2be1c32a0a21bca0f37b6fc
strvworks <strv13570@gmail.com>
2019-08-24 04:49:42 +0900

    fix

--- ballVecDetector.py
+++ ballVecDetector.py
@@ -16,7 +16,7 @@ def loop():
     GRID_WIDTH = 40
     CIRCLE_RADIUS = 2
 
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(-1)
 
     cv2.namedWindow('motion')
     ret, frame_next = cap.read()
@@ -38,32 +38,20 @@ def loop():
 
     while True:
         frame_number += 1
-        # フレーム間の差分計算
+
+        if not detecting and frame_number % 15 != 0:
+            continue
+
         color_diff = cv2.absdiff(frame_next, frame_pre)
-        # グレースケール変換
         gray_diff = cv2.cvtColor(color_diff, cv2.COLOR_BGR2GRAY)
-        # ２値化
         retval, black_diff = cv2.threshold(gray_diff, 30, 1, cv2.THRESH_BINARY)
-        # プロセッサ処理時間(sec)を取得
         proc_time = time.clock()
-        # モーション履歴画像の更新
         cv2.motempl.updateMotionHistory(black_diff, motion_history, proc_time, DURATION)
-        # 古いモーションの表示を経過時間に応じて薄くする
         hist_color = np.array(np.clip((motion_history - (proc_time - DURATION)) / DURATION, 0, 1) * 255, np.uint8)
-        # グレースケール変換
         hist_gray = cv2.cvtColor(hist_color, cv2.COLOR_GRAY2BGR)
-
-        # モーションの変化率を計算
         movements_level = gray_diff.sum()
-        # print(movements_level)
-
-        # モーション履歴画像の変化方向の計算
-        #   ※ orientationには各座標に対して変化方向の値（deg）が格納されます
         mask, orientation = cv2.motempl.calcMotionGradient(motion_history, 0.25, 0.05, apertureSize = 5)
-
-        # 全体的なモーション方向を計算
         angle_deg = cv2.motempl.calcGlobalOrientation(orientation, mask, motion_history, proc_time, DURATION)
-        # 全体の動きを黄色い線で描画
         cv2.circle(hist_gray,
                    (int(width / 2), int(height / 2)),
                    CIRCLE_RADIUS,
@@ -102,28 +90,10 @@ def loop():
                 f.write(str(detected_speed)+'\n')
                 f.write(str(detected_rad_average)+'\n')
 
-        # 各座標の動きを緑色の線で描画
         width_i = GRID_WIDTH
         while width_i < width:
             height_i = GRID_WIDTH
             while height_i < height:
-                # cv2.circle(hist_gray,
-                #             (width_i, height_i),
-                #             CIRCLE_RADIUS,
-                #             (0, 255, 0),
-                #             2,
-                #             16,
-                #             0)
-                # angle_deg = orientation[height_i - 1][width_i - 1]
-                # if angle_deg > 0:
-                #     angle_rad = math.radians(angle_deg)
-                #     cv2.line(hist_gray,
-                #             (width_i, height_i),
-                #             (int(width_i + math.cos(angle_rad) * LINE_LENGTH_GRID), int(height_i + math.sin(angle_rad) * LINE_LENGTH_GRID)),
-                #             (0, 255, 0),
-                #             2,
-                #             16,
-                #             0)
                 if frame_number % 2 == 0:
                     move_points = np.where(orientation > 10)
                     point_x = np.nanmean(move_points[0])
@@ -143,17 +113,6 @@ def loop():
                             2,
                             16,
                             0)
-                try:
-                    cv2.line(hist_gray,
-                             (point_y, point_x),
-                             (0, int(math.tan(angle_rad)*(-point_y)+point_x)),
-                             (0, 215, 0),
-                             2,
-                             16,
-                             0)
-                except:
-                    pass
-
                 height_i += GRID_WIDTH
             width_i += GRID_WIDTH
 
@@ -178,4 +137,5 @@ def loop():
     cap.release()
     return
 
-loop()
+if __name__=='__main__':
+    loop()
